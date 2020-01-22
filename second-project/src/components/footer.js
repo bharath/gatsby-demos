0c45e52b8f50daf653e300b2ec403f724e49ee57
Bharath <obharath@gmail.com>
2020-01-22 23:21:06 +0530

    Initial commit

---
+++ second-project/src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+
+	<footer>
+		<hr />
+		<p>Copyright 2020 Bharath.</p>
+	</footer>
+
+)
---
+++ second-project/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+export default () => (
+
+	<Layout>
+		<h1>Hello World</h1>
+		<p>
+			Read more <Link to={"/about"}>about me</Link>
+		</p>
+	</Layout>
+
+)
