f9f013c4ceab12b3faa2d3709fe56bce06112ddf
Bharath <obharath@gmail.com>
2020-01-23 20:42:31 +0530

    Working with Images in Gatsby

--- mdx-blog/src/pages/index.js
+++ mdx-blog/src/pages/index.js
@@ -3,11 +3,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import gatsbyAstronaut from "../images/gatsby-astronaut.png"
+
 export default () => (
 
 	<Layout>
 		<SEO title="Welcome to our Site!" />
 		<h1>Hello World</h1>
+		<img src={gatsbyAstronaut} alt="A Gatsby Astronaut" style={{width: `200px`}}/>
 		<p>
 			Read more <Link to={"/about"}>about me</Link>
 		</p>
